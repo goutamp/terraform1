59665c51a2a2db3c2532a912c2e1a794191a841b
gpramanick <gpramanick@beyondtrust.com>
2022-03-28 13:04:47 +0530

    add the terraform file

---
+++ main.tf
@@ -0,0 +1,30 @@
+
+terraform {
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source = "hashicorp/aws"
+    }
+  }
+  backend "s3" {
+    bucket = "goutam-friday"
+    key    = "tftoday"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "iam_user" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
+  version = "~> 4"
+
+  name          = "vasya.pupkin"
+  force_destroy = true
+
+  pgp_key = "keybase:test"
+
+  password_reset_required = false
+}
